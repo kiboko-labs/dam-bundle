45925277e5250dfe3e2b8788b6048b33da5da10f
Grégory Planchat <gregory@kiboko.fr>
2019-01-17 11:36:12 +0100

    Moved a data transformer for document node UUID

--- src/DAMBundle/Form/Type/DocumentNodeUuidType.php
+++ src/DAMBundle/Form/Type/DocumentNodeUuidType.php
@@ -2,6 +2,7 @@
 
 namespace Kiboko\Bundle\DAMBundle\Form\Type;
 
+use DAMBundle\Form\DataTransformer\DocumentNodeUuidDataTransformer;
 use Kiboko\Bundle\DAMBundle\Entity\DocumentNode;
 use Ramsey\Uuid\UuidFactory;
 use Ramsey\Uuid\UuidInterface;
@@ -25,42 +26,7 @@ class DocumentNodeUuidType extends AbstractType
                         'readonly' => true,
                     ],
                 ]
-            )->addModelTransformer(new class implements DataTransformerInterface {
-                public function transform($value)
-                {
-                    if ($value === null) {
-                        return null;
-                    }
-                    if (!$value instanceof UuidInterface) {
-                        throw new \UnexpectedValueException(strtr(
-                            'Expected a %expected% instance, but got a %actual%.',
-                            [
-                                '%expected%' => UuidInterface::class,
-                                '%actual%' => is_object($value) ? get_class($value) : gettype($value),
-                            ]
-                        ));
-                    }
-
-                    return (string) $value;
-                }
-
-                public function reverseTransform($value)
-                {
-                    if ($value === null) {
-                        return null;
-                    }
-                    if (!is_string($value)) {
-                        throw new \UnexpectedValueException(strtr(
-                            'Expected a string, but got a %actual%.',
-                            [
-                                '%actual%' => is_object($value) ? get_class($value) : gettype($value),
-                            ]
-                        ));
-                    }
-
-                    return (new UuidFactory())->fromString($value);
-                }
-            })
+            )->addModelTransformer(new DocumentNodeUuidDataTransformer())
         );
 
         $builder->remove('owner');
